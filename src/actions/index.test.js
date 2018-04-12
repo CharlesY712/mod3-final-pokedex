de06acdb74f82c1f201bcdf61139a0e9695a4f5f
Charles Yach <CharlesY712@gmail.com>
2018-04-12 12:56:15 -0600

    Adding addPOkemonReducer test

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -3,10 +3,11 @@ import * as actions from './index.js';
 describe('Actions', () => {
 
   it('creates a type of ADD_POKEMON', () => {
-    const pokemon = []
+    const pokemon = [];
     const expected = {
       type: 'ADD_POKEMON',
       pokemon
-    }
-    expect()
-  };
+    };
+    expect(actions.addPokemon(pokemon)).toEqual(expected)
+  });
+});
